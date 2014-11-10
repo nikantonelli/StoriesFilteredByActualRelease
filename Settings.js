4e8ffb1763cd47fa083a2c6f6abaa85597168de3
nikantonelli <nik@nikantonelli.com>
2014-11-10 17:35:00 +0000

    Modify field selections
    Now can select other fields (whether useful or not)…

--- Settings.js
+++ Settings.js
@@ -21,7 +21,7 @@ Ext.define('Rally.apps.storyfieldfilter.Settings', {
                     ready: function(combo) {
                         combo.store.filterBy(function(record) {
                             var attr = record.get('fieldDefinition').attributeDefinition;
-                            return attr && !attr.ReadOnly && attr.Constrained && attr.AttributeType !== 'OBJECT' && attr.AttributeType !== 'COLLECTION';
+                            return attr && attr.Filterable && attr.AttributeType !== 'OBJECT' && attr.AttributeType !== 'COLLECTION';
                         });
                         if (combo.getRecord()) {
                             this.fireEvent('fieldselected', combo.getRecord());
